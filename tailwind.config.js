9a53a1c739320c6e3bcbc39b76f4caf67c8d10e4
Petr Kuzora <petr1725@yandex.ru>
2024-05-31 23:52:47 +0300

    Начало работы

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors(theme) {
+        return {
+          primary: {
+            DEFAULT: 'hsl(263, 55%, 52%)',
+            dark: 'hsl(217, 19%, 35%)',
+            blackish: 'hsl(219, 29%, 14%)',
+          },
+          gray: {
+            ...theme.colors.gray,
+            100: 'hsl(0, 0%, 81%)',
+            200: 'hsl(210, 46%, 95%)',
+          }
+        }
+      }
+    },
+  },
+  plugins: [],
+};
